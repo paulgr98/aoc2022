a549dae91c4ee7e924ee25ba0b83f784169bf83c
paulgr98 <pawelwnuk98@gmail.com>
2022-12-20 20:30:15 +0100

    Completed part 1 of day 8

--- Day8/day8.py
+++ Day8/day8.py
@@ -7,7 +7,7 @@
 # All trees on the edge of the grid are visible,
 # so we can just add the number of trees on the edge of the grid to the answer.
 
-with open('test.txt') as f:
+with open('input.txt') as f:
     data = [list(map(int, line.strip())) for line in f]
 
 
@@ -15,13 +15,13 @@ with open('test.txt') as f:
 # Find the number of visible trees in the forest.
 def find_number_of_visible_trees(forest: list) -> int:
     edge_trees = find_number_of_trees_on_edges(forest)
+    print(f'Number of trees on the edge of the forest: {edge_trees}')
     trees_horizontally = find_trees_horizontally(forest)
     # transpose the forest
     forest_transposed = list(map(list, zip(*forest)))
     trees_vertically = find_trees_horizontally(forest_transposed)
     trees_vertically = [(y, x) for x, y in trees_vertically]
     total_trees = set(trees_horizontally + trees_vertically)
-    print(sorted(total_trees))
 
     return len(total_trees) + edge_trees
 
@@ -33,8 +33,9 @@ def find_number_of_trees_on_edges(forest: list) -> int:
     visible_trees += len(forest[-1])
     # Check left and right edges (excluding corners)
     for row in forest[1:-1]:
-        visible_trees += 1 if row[0] else 0
-        visible_trees += 1 if row[-1] else 0
+        for i, col in enumerate(row):
+            if i == 0 or i == len(row) - 1:
+                visible_trees += 1
     return visible_trees
 
 
@@ -63,7 +64,7 @@ def find_trees_left_for_row_number(row: list, row_number) -> list:
 
 def find_trees_right_for_row_number(row: list, row_number) -> list:
     visible_trees = []
-    highest_tree = max(row)
+    highest_tree = max(row[1:-1])
     last_tree = row[-1]
     for index in range(len(row) - 2, 0, -1):
         if row[index] > last_tree:
